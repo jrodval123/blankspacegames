3f6c57fbd04dff921ea1a16342e23794592fa52c
Jesus Rodriguez <jesusrodriguez@Jesuss-MacBook-Pro-2.local>
2020-02-29 17:20:15 -0600

    on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,13 @@ function Navbar() {
       <div className="row">
         <img src={require("../assets/logo.png")} alt="asset1"></img>
         <hr width="90%"></hr>
-        <h2 className="navitem">Game Concept</h2>
-        <h2 className="navitem">Art Style</h2>
-        <h2 className="navitem">Game Mechanics</h2>
-        <h2 className="navitem">Meet the Team</h2>
-        <h2 className="navitem">Contact</h2>
+        <h2 className="navitem"><a href={`https:/${"jrodval123.github.io/blankspacegames"}`}>Game Concept</a></h2>
+        <h2 className="navitem"><a href={`https:/${"jrodval123.github.io/blankspacegames"}`}>Art Style</a></h2>
+        <h2 className="navitem"><a href={`https:/${"jrodval123.github.io/blankspacegames"}`}>Game Mechanics</a></h2>
+        <h2 className="navitem"><a href={`https:/${"jrodval123.github.io/blankspacegames"}`}>Meet the Team</a></h2>
+        {/* <h2 className="navitem"><a href={`https:/${"jrodval123.github.io/blankspacegames"}`}>Contact</a>Contact</h2> */}
       </div>
     </aside>
   );
 }
-
 export default Navbar;
